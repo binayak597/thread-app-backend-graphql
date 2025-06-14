2479ab0ed76715bd33c14a89a13bf50f9d8a3886
Binayak Mukherjee <binayak.mukherjee9640@gmail.com>
2025-06-14 18:43:48 +0530

    integrate dev and prisma setup for graphql operation

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { ApolloServer } from "@apollo/server";
 import {startStandaloneServer} from "@apollo/server/standalone";
+import { prismaClient } from "./lib/db.js";
 
 
 
@@ -12,12 +13,38 @@ async function startServer() {
         hello: String
         sayName(name: String!): String
       }
+
+      type Mutation {
+        createUser(firstName: String!, lastName: String!, email: String!, password: String!, saltRound: String!): Boolean
+      }
     `,
     resolvers: {
 
       Query: {
         hello: () => "heyy there... i am a graphql server",
         sayName: (parent, {name}: {name: string}) => `heyy ${name}! what are you doing???`
+      },
+
+      Mutation: {
+        createUser: async (_, {firstName, lastName, email, password, saltRound}: {
+          firstName: string,
+          lastName: string,
+          email: string,
+          password: string, 
+          saltRound: string
+        }) => {
+
+          await prismaClient.user.create({
+            data: {
+              firstName,
+              lastName,
+              email,
+              password,
+              saltRound: "random_salt"
+            }
+          });
+          return true;
+        }
       }
 
     }
